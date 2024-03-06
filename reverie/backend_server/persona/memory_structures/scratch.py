16512e34baca1ee5d690280211479b7216cfff5f
cato-droid <76058138+cato-droid@users.noreply.github.com>
2024-03-06 16:50:42 +0100

    went back to older version of prompt
    new_currently was empty in newer versions and I don't know why

--- reverie/backend_server/persona/memory_structures/scratch.py
+++ reverie/backend_server/persona/memory_structures/scratch.py
@@ -186,14 +186,11 @@ class Scratch:
 
       currently_prompt = f"Given what {self.first_name} {self.last_name} is up to currently:\n"
       currently_prompt += scratch_load["currently"] + "\n\n"
-      currently_prompt += f"add this to it in the same style (talking about them in the third person) and give me back only that finished text: \n"
-      currently_prompt += f"{currently_input}\n"
-      print("\nprompt to llm:\n")
+      currently_prompt += f"add this to it in the same style (talking about them in the third person) and give my back only that finished text\n"
+      currently_prompt += f"{currently_input}\n\n"
       print(currently_prompt)
       new_currently = ChatGPT_single_request(currently_prompt)
-      print("\nits response:\n")
       print(new_currently)
-      print("\n\n")
       self.currently = new_currently
 
 
